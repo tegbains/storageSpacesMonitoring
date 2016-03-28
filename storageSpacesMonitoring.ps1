5e970bcbe7402b7b0efe9dd6111539b27ed1f450
Teg Bains <tegbains@tegbains.com>
2016-03-27 22:08:35 -0700

    Changed to use variables

--- storageSpacesMonitoring.ps1
+++ storageSpacesMonitoring.ps1
@@ -1,9 +1,17 @@
-﻿$storageSpacesStatus = Get-Disk | Where-Object BusType -Eq Spaces 
+﻿# The monitoring variables
+
+$mailServerName = "mail.shuttlesite.com"
+$emailAddressNotifyDestination = "notify_ss@ajayian.com"
+$emailAddressNotifySource = "notify_ss@ajayian.com"
+
+$emailSubject = "[Storage Spaces Monitoring] $env:computername"
+
+# grab the current Disk status and filter out all non-StorageSpaces disks
+$storageSpacesStatus = Get-StoragePool  
 
 
 $storageSpacesStatusList = $storageSpacesStatus |
 Format-List -Property FriendlyName, HealthStatus, Size, OperationalStatus | Out-String
 
 
-
-Send-MailMessage  -From notify_ss@ajayian.com  -To notify_ss@ajayian.com -Subject "[Storage Spaces Monitoring] $env:computername" -Body "$storageSpacesStatusList" -Smtpserver mail.shuttlesite.com
+Send-MailMessage  -From $emailAddressNotifySource  -To $emailAddressNotifySource -Subject $emailSubject -Body "$storageSpacesStatusList" -Smtpserver $mailServerName
